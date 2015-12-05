4defef44d06f5a732887181b119ac342fa8b8875
sukki89 <sukki89@gmail.com>
2015-12-04 17:57:20 -0800

    decision tree updates

--- TestLogitReg.py
+++ TestLogitReg.py
@@ -1,4 +1,6 @@
 from pandas import Series, DataFrame
+
+from sklearn import tree
 import pandas as pd
 import numpy as np
 import nltk
@@ -14,7 +16,7 @@ from sklearn.linear_model import LogisticRegression
 def computeAccuracy(Y,YPredicted):
     count = 0
     for i in range(0,len(Y)):
-        if Y[i] == YPredicted[i].encode('ascii','ignore'):
+        if Y[i] == YPredicted[i]:
             count = count + 1
 
     return (count/len(Y) * 100)
@@ -24,6 +26,7 @@ def computeAccuracy(Y,YPredicted):
 traindf = pd.read_json("resources/train.json")
 traindf['ingredients_clean_string'] = [' , '.join(z).strip() for z in traindf['ingredients']]
 traindf['ingredients_string'] = [' '.join([WordNetLemmatizer().lemmatize(re.sub('[^A-Za-z]', ' ', line)) for line in lists]).strip() for lists in traindf['ingredients']]
+
 
 testdf = pd.read_json("resources/test.json")
 testdf['ingredients_clean_string'] = [' , '.join(z).strip() for z in testdf['ingredients']]
@@ -46,28 +49,35 @@ targets_tr = traindf['cuisine']
 
 predictors_ts = tfidfts
 
-
-#classifier = LinearSVC(C=0.80, penalty="l2", dual=False)
+# LR, SCV
+classifier = LinearSVC(C=0.80, penalty="l2", dual=False)
 parameters = {'C':[1, 10]}
-#clf = LinearSVC()
+clf = LinearSVC()
 clf = LogisticRegression()
-
 classifier = grid_search.GridSearchCV(clf, parameters)
-
 classifier=classifier.fit(predictors_tr,targets_tr)
+
+#decision trees
+#clf = tree.DecisionTreeClassifier()
+#parameters = {'max_depth':[100]}
+#classifier=clf.fit(predictors_tr,targets_tr)
 
 predictions_train = classifier.predict(predictors_tr)
 
 predictions=classifier.predict(predictors_ts)
 for i in range(0,predictions.size):
-    predictions[i] = predictions[i].encode('ascii','ignore')
+    predictions[i] = str(predictions[i])
+for i in range(0,predictions_train.size):
+    predictions_train[i] = str(predictions_train[i])
+
+#print predictions_train
 testdf['cuisine'] = predictions
 testdf = testdf.sort('id' , ascending=True)
 
-print computeAccuracy(predictors_tr,predictions_train)
+#print computeAccuracy(predictors_tr,predictions_train)
 #print predictions_train
 #print computeAccuracy(predictions,targets_ts)
 
-print testdf
-testdf[['id' , 'ingredients_clean_string' , 'cuisine' ]].to_csv("submission.csv")
+#print testdf
 
+testdf[['id' , 'cuisine' ]].to_csv("subTree.csv");
